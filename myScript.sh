2068308740fe7cd3c25e97bed1ce765139c0646e
Shubham Baliyan <shubham_baliyan@epam.com>
2022-02-09 21:46:50 +0530

    script changes again

--- myScript.sh
+++ myScript.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
- npm install
+npm install
 sudo rm -r dist/
- npm run build --prod
+npm run build --prod
 sudo rm -r /var/www/$1
 sudo cp -r dist/ /var/www/$1
 echo "$1 done"
-echo "hi"
+echo "build ready"
