7af24cde85eb23db8d5583818747332fec906514
Shubham Baliyan <shubham_baliyan@epam.com>
2022-03-03 12:15:00 +0530

    my order component

--- src/app/myorder/myorder.component.ts
+++ src/app/myorder/myorder.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { CheckoutService } from '../checkout.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-myorder',
@@ -12,8 +13,25 @@ export class MyorderComponent implements OnInit {
     private checkoutService: CheckoutService,
     private authService: AuthService
   ) {}
+  moment = moment;
   user: any;
   orders: any;
+  getStatus(date: string) {
+    date = moment(date).format('DD/MM/YY');
+    let today = moment().format('DD/MM/YY');
+    console.log(date, today);
+    if (date === today) {
+      return 'Order Placed';
+    }
+    if (moment(date, 'DD/MM/YY').add(1, 'days').format('DD/MM/YY') === today) {
+      return 'Shipped';
+    }
+    if (moment(date, 'DD/MM/YY').add(2, 'days').format('DD/MM/YY') === today) {
+      return 'Out for delivery';
+    }
+
+    return 'Delivered';
+  }
   ngOnInit(): void {
     this.user = this.authService.getUser();
     this.checkoutService.getOrderDataByUser(this.user.id).subscribe((res) => {
